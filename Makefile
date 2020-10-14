00253a4fa2fb2cdc1a22163b8c267c0175f1b3b1
Paul Downey <psd@whatfettle.com>
2020-10-14 09:59:51 +0100

    Pipeline makerules

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+# currently only supports one pipeline per-makefile ..
+PIPELINE_NAME=conservation-area-geography
+
 include makerules/makerules.mk
 include makerules/development.mk
 include makerules/collection.mk
--- makerules/pipeline.mk
+++ makerules/pipeline.mk
@@ -18,7 +18,11 @@ ifeq ($(COLLECTION_DIR),)
 COLLECTION_DIR=collection/
 endif
 
-ifeq ($(PIPELINE_RESOURCE_FILES),)
+ifeq ($(RESOURCE_DIR),)
+RESOURCE_DIR:=collection/resource/
+endif
+
+ifeq ($(RESOURCE_FILES),)
 RESOURCE_FILES:=$(wildcard $(RESOURCE_DIR)*)
 endif
 
@@ -26,10 +30,11 @@ second-pass::
 	@$(MAKE) --no-print-directory pipeline
 
 # restart the make process to pick-up collected files
-pipeline::
+pipeline::	normalise
+
 
 #
-#  convert resources into CSV
+#  convert collected resources into CSV
 #
 CONVERTED_DIR=var/converted/
 CONVERTED_FILES  := $(addsuffix .csv,$(subst $(RESOURCE_DIR),$(CONVERTED_DIR),$(RESOURCE_FILES)))
@@ -45,10 +50,68 @@ FIXED_CONVERTED_FILES:=$(subst $(FIXED_DIR),$(CONVERTED_DIR),$(FIXED_FILES))
 
 $(FIXED_CONVERTED_FILES):
 	@mkdir -p $(CONVERTED_DIR)
-	digital-land convert $(subst $(CONVERTED_DIR),$(FIXED_DIR),$@) $@
+	digital-land --pipeline-name $(PIPELINE_NAME) convert $(subst $(CONVERTED_DIR),$(FIXED_DIR),$@) $@
+	@rm -f collection/resource/*.gfs
 
 convert: $(CONVERTED_FILES)
 	@:
+
+
+#
+#  normalise CSV file spacing
+#
+NORMALISED_DIR=var/normalised/
+NORMALISED_FILES  := $(subst $(CONVERTED_DIR),$(NORMALISED_DIR),$(CONVERTED_FILES))
+
+$(NORMALISED_DIR)%: $(CONVERTED_DIR)%
+	@mkdir -p $(NORMALISED_DIR)
+	digital-land --pipeline-name $(PIPELINE_NAME) normalise  $< $@
+
+normalise: $(NORMALISED_FILES)
+	@:
+
+
+#
+#  map column names to specification
+#
+MAPPED_DIR=var/mapped/
+MAPPED_FILES  := $(subst $(NORMALISED_DIR),$(MAPPED_DIR),$(NORMALISED_FILES))
+
+$(MAPPED_DIR)%: $(NORMALISED_DIR)% pipeline/column.csv
+	@mkdir -p $(MAPPED_DIR)
+	digital-land --pipeline-name $(PIPELINE_NAME) map $< $@
+
+map: $(MAPPED_FILES)
+	@:
+
+
+#
+#  harmonise field values
+#
+HARMONISED_DIR=var/harmonised/
+HARMONISED_FILES  := $(subst $(MAPPED_DIR),$(HARMONISED_DIR),$(MAPPED_FILES))
+
+$(HARMONISED_DIR)%: $(MAPPED_DIR)%
+	@mkdir -p $(HARMONISED_DIR)
+	digital-land normalise  $< $@
+
+harmonise: $(HARMONISED_FILES)
+	@:
+
+
+#
+#  transform CSV fields into latest model
+#
+TRANSFORMED_DIR=var/transformed/
+TRANSFORMED_FILES  := $(subst $(HARMONISED_DIR),$(TRANSFORMED_DIR),$(HARMONISED_FILES))
+
+$(TRANSFORMED_DIR)%: $(HARMONISED_DIR)%
+	@mkdir -p $(TRANSFORMED_DIR)
+	digital-land normalise  $< $@
+
+transform: $(TRANSFORMED_FILES)
+	@:
+
 
 # update makerules from source
 update::
