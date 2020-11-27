4d2f43ff57d0411287029f2acfb4dcdab42fdecd
Jon Bramley <brammers@gmail.com>
2020-11-27 13:45:35 +0000

    fix: include geospatial.mk

---
+++ makerules/geospatial.mk
@@ -0,0 +1,14 @@
+GDAL := $(shell command -v ogr2ogr 2> /dev/null)
+UNAME := $(shell uname)
+init::
+ifndef GDAL
+ifeq ($(UNAME),Darwin)
+$(error GDAL tools not found in PATH)
+endif
+	sufo apt-get update
+	sudo apt-get install gdal-bin
+endif
+
+# update makerules from source
+update::
+	curl -qsL '$(SOURCE_URL)/makerules/master/geospatial.mk' > makerules/geospatial.mk
