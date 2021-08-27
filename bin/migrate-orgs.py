0c4e2c84dae5f46d22a0348d00e7a81483415f3a
Paul Downey <psd@whatfettle.com>
2021-08-27 15:48:58 +0100

    Counties and combined authorities aren't LPAs

--- bin/migrate-orgs.py
+++ bin/migrate-orgs.py
@@ -17,11 +17,12 @@ w = csv.DictWriter(open("/tmp/source.csv", "w", newline=""), fieldnames=fieldnam
 for row in csv.DictReader(open("var/cache/organisation.csv", newline="")):
     organisation = row["organisation"]
     if organisation.split(":")[0] in ["local-authority-eng", "development-corporation", "national-park"]:
-        if organisation not in source:
-            o = {}
-            o["organisation"] = organisation
-            o["collection"] = last["collection"]
-            o["pipelines"] = last["pipelines"]
-            o["entry-date"] = datetime.utcnow().isoformat()[:-3]+'Z'
-            o["end-date"] = row["end-date"]
-            w.writerow(o)
+        if row["local-authority-type"] not in ["CTY", "COMB"]:
+            if organisation not in source:
+                o = {}
+                o["organisation"] = organisation
+                o["collection"] = last["collection"]
+                o["pipelines"] = last["pipelines"]
+                o["entry-date"] = datetime.utcnow().isoformat()[:-3]+'Z'
+                o["end-date"] = row["end-date"]
+                w.writerow(o)
